02537be1529beef82aa218e0fb364a17e48464bb
GoodVessel92551 <robininness@gmail.com>
2025-02-06 20:32:41 +0000

    Fix

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ function handleOrientation(event) {
     let gamma_difference = event.gamma - gamma_value;
     gamma_value = event.gamma;
     console.log(gamma_difference);
-    const gamma = event.gamma;
+    let gamma = event.gamma;
+    console.log((gamma > 70 || gamma <-70))
     if (gamma > 70 || gamma <-70){
       document.body.style.backgroundColor = "blue";
-    }else if (gamma > 0){
+    }else if (gamma > 0 && gamma < 70){
       document.body.style.backgroundColor = "red";
     }
-    else if (gamma < 0){
+    else if (gamma < 0 && gamma > -70){
       document.body.style.backgroundColor = "green";
     }
     else{

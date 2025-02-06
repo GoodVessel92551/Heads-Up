468cff21a060aede58cef40ff9afb3dc967b3b37
GoodVessel92551 <robininness@gmail.com>
2025-02-06 20:16:15 +0000

    Added Color

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ window.addEventListener("deviceorientation", handleOrientation, true);
 
 
 const gammaElement = document.getElementById("gammaElement");
-
+let gamma_value = 0;
 function handleOrientation(event) {
+    let gamma_difference = event.gamma - gamma_value;
+    if(gamma_difference > 5){
+        document.body.style.backgroundColor = "red";
+    }
+    else if(gamma_difference < -5){
+        document.body.style.backgroundColor = "green";
+    }
+
     const gamma = event.gamma;
     gammaElement.innerHTML = "Gamma: "+gamma;
   }

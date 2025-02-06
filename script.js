ec7feca385c4757a2a58cdcede640feda93da87b
GoodVessel92551 <robininness@gmail.com>
2025-02-06 20:26:43 +0000

    Dynamic Color

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const gammaElement = document.getElementById("gammaElement");
 let gamma_value = 0;
 function handleOrientation(event) {
     let gamma_difference = event.gamma - gamma_value;
-    if(gamma_difference > 5){
-        document.body.style.backgroundColor = "red";
-    }
-    else if(gamma_difference < -5){
-        document.body.style.backgroundColor = "green";
-    }
-
+    gamma_value = event.gamma;
+    console.log(gamma_difference);
     const gamma = event.gamma;
+    if (gamma > 70 || gamma <-70){
+      document.body.style.backgroundColor = "blue";
+    }else if (gamma > 0){
+      document.body.style.backgroundColor = "red";
+    }
+    else if (gamma < 0){
+      document.body.style.backgroundColor = "green";
+    }
+    else{
+      document.body.style.backgroundColor = "white";
+    }
     gammaElement.innerHTML = "Gamma: "+gamma;
   }
